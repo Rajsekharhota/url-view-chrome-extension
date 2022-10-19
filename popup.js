cfaed77e8a14fd9ed592c7aa9a4617c7149c7271
Rajsekhar <rajsekharhota@gmail.com>
2022-10-19 22:55:35 +0530

    first commit

---
+++ popup.js
@@ -0,0 +1,14 @@
+const btn = document.querySelector(".displayUrlBtn");
+
+btn.addEventListener("click", async () => {
+  chrome.storage.sync.get("url", ({ url }) => {
+    console.log("url: ", url);
+  });
+
+  chrome.tabs.query(
+    { active: true, windowId: chrome.windows.WINDOW_ID_CURRENT },
+    function (tabs) {
+      alert(tabs[0].url);
+    }
+  );
+});
